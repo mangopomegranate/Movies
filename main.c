8b5fb25dbe9d2b47b4a15b273da4cc16aff7c831
mangopomegranate <75091890+mangopomegranate@users.noreply.github.com>
2021-01-10 18:42:03 -0800

    Fresh Files

---
+++ main.c
@@ -0,0 +1,18 @@
+// Author: Abhash Sharma
+// Course: CS344 Winter 2021
+// Program Description: Movies Information
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Structure for Movie Information */
+struct movie
+{
+
+};
+
+int main(void) {
+	printf("Hello World\n");
+	return 0;
+};
